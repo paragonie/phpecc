55f54f8fe520b563b38f8108ae3d07a4db463df8
Paragon Initiative Enterprises <security@paragonie.com>
2024-04-23 19:56:26 -0400

    Fix type annotations for RNG class

--- src/Random/RandomNumberGenerator.php
+++ src/Random/RandomNumberGenerator.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Mdanter\Ecc\Random;
 
+use Exception;
+use GMP;
 use Mdanter\Ecc\Math\GmpMathInterface;
 use Mdanter\Ecc\Util\NumberSize;
 
@@ -23,10 +25,11 @@ class RandomNumberGenerator implements RandomNumberGeneratorInterface
     }
 
     /**
-     * @param \GMP $max
-     * @return \GMP
+     * @param GMP $max
+     * @return GMP
+     * @throws Exception
      */
-    public function generate(\GMP $max): \GMP
+    public function generate(GMP $max): GMP
     {
         $numBits = NumberSize::bnNumBits($this->adapter, $max);
         $numBytes = (int) ceil($numBits / 8);
@@ -35,6 +38,7 @@ class RandomNumberGenerator implements RandomNumberGeneratorInterface
         $value = $this->adapter->stringToInt($bytes);
 
         $mask = gmp_sub(gmp_pow(2, $numBits), 1);
+        /** @var GMP $integer */
         $integer = gmp_and($value, $mask);
 
         return $integer;
